a33262002aef1112d4d42c184e2f12e6125ff084
zaydon369 <zaydon369@yahoo.com>
2015-11-26 13:22:54 +0800

    完成欢迎页面

---
+++ app/src/main/java/com/itzheng/smartbeijing/welcome/GuideActivity.java
@@ -0,0 +1,11 @@
+package com.itzheng.smartbeijing.welcome;
+
+import android.app.Activity;
+
+/**
+ * Created by asus on 2015/11/26.
+ * 引导页
+ */
+public class GuideActivity extends Activity{
+
+}
---
+++ app/src/main/java/com/itzheng/smartbeijing/welcome/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.itzheng.smartbeijing.welcome;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import com.itzheng.smartbeijing.MainActivity;
+import com.itzheng.smartbeijing.R;
+
+/**
+ * Created by asus on 2015/11/26.
+ * Splash界面,用于初始化整个应用程序
+ */
+public class SplashActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //添加布局文件
+        setContentView(R.layout.activity_splash);
+        //开启子线程,1秒钟后进入指定页面
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(1000);
+                enterActivity(true);
+            }
+        }).start();
+
+    }
+
+    /**
+     * 根据是否第一次进入,跳转到不同页面
+     */
+    private void enterActivity(boolean isFirst) {
+        if(isFirst){
+            //第一次进入,跳到导航页
+            Intent intent =new Intent
+                    (SplashActivity.this,GuideActivity.class);
+            startActivity(intent);
+        }else{
+            //不是第一次进入,进入详情页
+            Intent intent=new Intent
+                    (SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
