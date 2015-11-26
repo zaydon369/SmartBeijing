4f2c457b8250e40834dc2eced6adbe69c5c2b325
zaydon369 <zaydon369@yahoo.com>
2015-11-26 13:35:55 +0800

    提交一些必要的文件

---
+++ README.md
@@ -0,0 +1,6 @@
+# 智慧北京(COPY)
+## day01
+### splash界面(SplashActivity.java)
+- ImageView + ShareUtil
+
+### 导航页面(viewPager)
--- app/src/main/java/com/itzheng/smartbeijing/welcome/GuideActivity.java
+++ app/src/main/java/com/itzheng/smartbeijing/welcome/GuideActivity.java
@@ -1,11 +1,20 @@
 package com.itzheng.smartbeijing.welcome;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.itzheng.smartbeijing.R;
 
 /**
  * Created by asus on 2015/11/26.
  * 引导页
  */
 public class GuideActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.guide);
 
+    }
 }
